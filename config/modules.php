3de15b6788513983d3124633cd61614372466fbf
abo3adel <abo3adel35@gmail.com>
2025-10-11 02:45:06 +0300

    feat: laravel modules user custom stubs

--- config/modules.php
+++ config/modules.php
@@ -7,7 +7,6 @@ use Nwidart\Modules\Activators\FileActivator;
 use Nwidart\Modules\Providers\ConsoleServiceProvider;
 
 return [
-
     /*
     |--------------------------------------------------------------------------
     | Module Namespace
@@ -27,13 +26,14 @@ return [
     |
     */
     'stubs' => [
-        'enabled' => false,
-        'path' => base_path('vendor/nwidart/laravel-modules/src/Commands/stubs'),
+        'enabled' => true,
+        'path' => base_path('stubs/nwidart-stubs'),
         'files' => [
             'routes/web' => 'routes/web.php',
             'routes/api' => 'routes/api.php',
             'views/index' => 'resources/views/index.blade.php',
-            'views/master' => 'resources/views/components/layouts/master.blade.php',
+            'views/master' =>
+                'resources/views/components/layouts/master.blade.php',
             'scaffold/config' => 'config/config.php',
             'composer' => 'composer.json',
             'assets/js/app' => 'resources/assets/js/app.js',
@@ -58,10 +58,30 @@ return [
              *
              * Note: Keys should be in UPPERCASE.
              */
-            'routes/web' => ['LOWER_NAME', 'STUDLY_NAME', 'PLURAL_LOWER_NAME', 'KEBAB_NAME', 'MODULE_NAMESPACE', 'CONTROLLER_NAMESPACE'],
-            'routes/api' => ['LOWER_NAME', 'STUDLY_NAME', 'PLURAL_LOWER_NAME', 'KEBAB_NAME', 'MODULE_NAMESPACE', 'CONTROLLER_NAMESPACE'],
+            'routes/web' => [
+                'LOWER_NAME',
+                'STUDLY_NAME',
+                'PLURAL_LOWER_NAME',
+                'KEBAB_NAME',
+                'MODULE_NAMESPACE',
+                'CONTROLLER_NAMESPACE',
+            ],
+            'routes/api' => [
+                'LOWER_NAME',
+                'STUDLY_NAME',
+                'PLURAL_LOWER_NAME',
+                'KEBAB_NAME',
+                'MODULE_NAMESPACE',
+                'CONTROLLER_NAMESPACE',
+            ],
             'vite' => ['LOWER_NAME', 'STUDLY_NAME', 'KEBAB_NAME'],
-            'json' => ['LOWER_NAME', 'STUDLY_NAME', 'KEBAB_NAME', 'MODULE_NAMESPACE', 'PROVIDER_NAMESPACE'],
+            'json' => [
+                'LOWER_NAME',
+                'STUDLY_NAME',
+                'KEBAB_NAME',
+                'MODULE_NAMESPACE',
+                'PROVIDER_NAMESPACE',
+            ],
             'views/index' => ['LOWER_NAME'],
             'views/master' => ['LOWER_NAME', 'STUDLY_NAME', 'KEBAB_NAME'],
             'scaffold/config' => ['STUDLY_NAME'],
@@ -135,7 +155,10 @@ return [
             'channels' => ['path' => 'app/Broadcasting', 'generate' => false],
             'class' => ['path' => 'app/Classes', 'generate' => false],
             'command' => ['path' => 'app/Console', 'generate' => false],
-            'component-class' => ['path' => 'app/View/Components', 'generate' => false],
+            'component-class' => [
+                'path' => 'app/View/Components',
+                'generate' => false,
+            ],
             'emails' => ['path' => 'app/Emails', 'generate' => false],
             'event' => ['path' => 'app/Events', 'generate' => false],
             'enums' => ['path' => 'app/Enums', 'generate' => false],
@@ -145,7 +168,10 @@ return [
             'interfaces' => ['path' => 'app/Interfaces', 'generate' => false],
             'listener' => ['path' => 'app/Listeners', 'generate' => false],
             'model' => ['path' => 'app/Models', 'generate' => false],
-            'notifications' => ['path' => 'app/Notifications', 'generate' => false],
+            'notifications' => [
+                'path' => 'app/Notifications',
+                'generate' => false,
+            ],
             'observer' => ['path' => 'app/Observers', 'generate' => false],
             'policies' => ['path' => 'app/Policies', 'generate' => false],
             'provider' => ['path' => 'app/Providers', 'generate' => true],
@@ -158,7 +184,10 @@ return [
             'traits' => ['path' => 'app/Traits', 'generate' => false],
 
             // app/Http/
-            'controller' => ['path' => 'app/Http/Controllers', 'generate' => true],
+            'controller' => [
+                'path' => 'app/Http/Controllers',
+                'generate' => true,
+            ],
             'filter' => ['path' => 'app/Http/Middleware', 'generate' => false],
             'request' => ['path' => 'app/Http/Requests', 'generate' => false],
 
@@ -167,7 +196,10 @@ return [
 
             // database/
             'factory' => ['path' => 'database/factories', 'generate' => true],
-            'migration' => ['path' => 'database/migrations', 'generate' => true],
+            'migration' => [
+                'path' => 'database/migrations',
+                'generate' => true,
+            ],
             'seeder' => ['path' => 'database/seeders', 'generate' => false],
 
             // lang/
@@ -175,7 +207,10 @@ return [
 
             // resource/
             'assets' => ['path' => 'resources/assets', 'generate' => false],
-            'component-view' => ['path' => 'resources/views/components', 'generate' => false],
+            'component-view' => [
+                'path' => 'resources/views/components',
+                'generate' => false,
+            ],
             'views' => ['path' => 'resources/views', 'generate' => false],
 
             // routes/
@@ -216,7 +251,6 @@ return [
         |
         */
         'translations' => false,
-
     ],
 
     /*
@@ -229,9 +263,8 @@ return [
     |
     */
     'commands' => ConsoleServiceProvider::defaultCommands()
-        ->merge([
-            ModuleDtoMakeCommand::class,
-        ])->toArray(),
+        ->merge([ModuleDtoMakeCommand::class])
+        ->toArray(),
 
     /*
     |--------------------------------------------------------------------------
@@ -244,9 +277,7 @@ return [
     */
     'scan' => [
         'enabled' => false,
-        'paths' => [
-            base_path('vendor/*/*'),
-        ],
+        'paths' => [base_path('vendor/*/*')],
     ],
 
     /*
