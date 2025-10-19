d0d885fbafd05c5929b582394021772b72a37b32
abo3adel <abo3adel35@gmail.com>
2025-10-20 00:26:47 +0300

    feat(genres): add module

---
+++ Modules/Genres/app/Http/Controllers/GenresController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Modules\Genres\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+
+class GenresController
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): JsonResponse
+    {
+        //
+
+        return response()->json([]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request): JsonResponse
+    {
+        //
+
+        return response()->json([]);
+    }
+
+    /**
+     * Show the specified resource.
+     */
+    public function show(mixed $id): JsonResponse
+    {
+        //
+
+        return response()->json([]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, mixed $id): JsonResponse
+    {
+        //
+
+        return response()->json([]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(mixed $id): JsonResponse
+    {
+        //
+
+        return response()->json([]);
+    }
+}
